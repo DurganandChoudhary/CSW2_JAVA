e1d46f872f249615724549f8aa76be8aaddc8bbe
DurganandChoudhary <102054803+DurganandChoudhary@users.noreply.github.com>
2023-04-14 11:07:09 +0530

    Add files via upload

--- Q2.java
+++ Q2.java
@@ -1,41 +1,13 @@
-
-class Student{
-	int age;
-	int mark; 
-	String name;
-	String sec;
-	
-	 public Student(int age,int mark,String name,String sec) {
-		this.age=age;
-		this.mark=mark;
-		this.name=name;
-		this.sec=sec;
-		 System.out.println("name "+name+"age "+age+"mark"+mark+"sec"+sec);
-		 
-	}
-	
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 public class Q2 {
-	public static void main(String[]args) {
-		
-	}
+    public static void main(String args[])
+    {
+        int arr[]={1,5,7,4,36,7,9,5,65};
+        int size=arr.length;
+        int sum=0;
+        for(int i=0;i<size;i++)
+        sum = sum+arr[i];
 
+        System.out.print("Sum = "+sum);
+    }
+    
 }
