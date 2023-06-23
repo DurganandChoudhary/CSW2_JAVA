3e707617a6f06ba48b94a07e9f771148dc198571
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-06-23 16:30:52 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,39 @@
+public class SelectionSort {
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            swap(arr, i, minIndex);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 12, 1, 6, 3, 9};
+        System.out.println("Before sorting:");
+        printArray(arr);
+        
+        selectionSort(arr);
+
+        System.out.println("After sorting:");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
