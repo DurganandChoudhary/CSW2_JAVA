f4249ff24795caf09b76e9ae1a16b7094f9069e5
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-06-23 16:35:03 +0530

    Add files via upload

---
+++ QuickSort.java
@@ -0,0 +1,53 @@
+public class QuickSort {
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int partitionIndex = partition(arr, low, high);
+            
+            quickSort(arr, low, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, high);
+        }
+    }
+    
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        
+        swap(arr, i + 1, high);
+        
+        return i + 1;
+    }
+    
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 12, 1, 6, 3, 9};
+        int n = arr.length;
+        
+        System.out.println("Before sorting:");
+        printArray(arr);
+        
+        quickSort(arr, 0, n - 1);
+
+        System.out.println("After sorting:");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
