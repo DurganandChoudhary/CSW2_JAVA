494750fbc958c04107df92740c0b37276422afdd
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-06-25 20:11:37 +0530

    Add files via upload

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,153 +1,64 @@
-package Binary_Tree;
+class Node {
+    int data;
+    Node left;
+    Node right;
 
-public class BinaryTree {
-    private Node root;
-
-    public void InsertNode(int value) {
-        root = InsertNode(root, value);
-    }
-
-    private Node InsertNode(Node node, int value) {
-        if (node == null) {
-            node = new Node(value);
-        } else {
-            if (node.value > value) {
-                node.lChild = InsertNode(node.lChild, value);
-            } else {
-                node.rChild = InsertNode(node.rChild, value);
-            }
-        }
-        return node;
-    }
-
-    public boolean Find(int value) {
-        Node curr = root;
-        while (curr != null) {
-            if (curr.value == value) {
-                return true;
-            } else if (curr.value > value) {
-                curr = curr.lChild;
-            } else {
-                curr = curr.rChild;
-            }
-        }
-        return false;
-    }
-
-    public int FindMin() {
-        Node node = root;
-        if (node == null) {
-            return Integer.MAX_VALUE;
-        }
-        while (node.lChild != null) {
-            node = node.lChild;
-        }
-        return node.value;
-    }
-
-    public Node FindMinNode(Node curr) {
-        Node node = curr;
-        if (node == null) {
-            return null;
-        }
-        while (node.lChild != null) {
-            node = node.lChild;
-        }
-        return node;
-    }
-
-    public Node FindMaxNode(Node curr) {
-        Node node = curr;
-        if (node == null) {
-            return null;
-        }
-        while (node.rChild != null) {
-            node = node.rChild;
-        }
-        return node;
-    }
-
-    public void deleteNode(int value) {
-        root = deleteNode(root, value);
-    }
-
-    private Node deleteNode(Node node, int value) {
-        if (node == null) {
-            return null;
-        }
-
-        if (value < node.value) {
-            node.lChild = deleteNode(node.lChild, value);
-        } else if (value > node.value) {
-            node.rChild = deleteNode(node.rChild, value);
-        } else {
-            if (node.lChild == null && node.rChild == null) {
-                node = null;
-            } else if (node.lChild == null) {
-                node = node.rChild;
-            } else if (node.rChild == null) {
-                node = node.lChild;
-            } else {
-                Node minNode = FindMinNode(node.rChild);
-                node.value = minNode.value;
-                node.rChild = deleteNode(node.rChild, minNode.value);
-            }
-        }
-
-        return node;
-    }
-    public boolean isBST3(Node root) {
-        if (root == null)
-            return true;
-        if (root.lChild != null && FindMaxNode(root.lChild).value > root.value)
-            return false;
-        if (root.rChild != null && FindMinNode(root.rChild).value <= root.value)
-            return false;
-        return (isBST3(root.lChild) && isBST3(root.rChild));
-    }
-
-    public static void main(String[] args) {
-        BinaryTree binaryTree = new BinaryTree();
-        binaryTree.InsertNode(4);
-        binaryTree.InsertNode(2);
-        binaryTree.InsertNode(1);
-        binaryTree.InsertNode(3);
-        binaryTree.InsertNode(6);
-        binaryTree.InsertNode(5);
-        binaryTree.InsertNode(7);
-
-        System.out.println("Binary Tree Created Successfully!");
-
-        int searchValue = 3;
-        boolean isFound = binaryTree.Find(searchValue);
-        System.out.println("Search for value " + searchValue + ": " + isFound);
-
-        int minValue = binaryTree.FindMin();
-        System.out.println("Minimum value in the tree: " + minValue);
-
-        Node minNode = binaryTree.FindMinNode(binaryTree.root);
-        System.out.println("Minimum node in the tree: " + minNode.value);
-
-        Node maxNode = binaryTree.FindMaxNode(binaryTree.root);
-        System.out.println("Maximum node in the tree: " + maxNode.value);
-
-        boolean isBST = binaryTree.isBST3(binaryTree.root);
-        System.out.println("Is the tree a Binary Search Tree? " + isBST);
-        
-        int deleteValue = 2;
-        binaryTree.deleteNode(deleteValue);
-        System.out.println("Node " + deleteValue + " deleted from the tree.");
+    public Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
     }
 }
 
-class Node {
-    int value;
-    Node lChild;
-    Node rChild;
+public class BinaryTree {
+    Node root;
 
-    public Node(int value) {
-        this.value = value;
-        this.lChild = null;
-        this.rChild = null;
+    public BinaryTree() {
+        root = null;
+    }
+
+    // Insert a new node in the binary tree
+    public void insert(int data) {
+        root = insertRecursive(root, data);
+    }
+
+    // Recursive method to insert a new node in the binary tree
+    private Node insertRecursive(Node current, int data) {
+        if (current == null) {
+            return new Node(data);
+        }
+
+        if (data < current.data) {
+            current.left = insertRecursive(current.left, data);
+        } else if (data > current.data) {
+            current.right = insertRecursive(current.right, data);
+        }
+
+        return current;
+    }
+
+    // Traverse the binary tree in-order (left-root-right)
+    public void inOrderTraversal(Node node) {
+        if (node != null) {
+            inOrderTraversal(node.left);
+            System.out.print(node.data + " ");
+            inOrderTraversal(node.right);
+        }
+    }
+
+    // Test the code
+    public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
+
+        // Insert nodes into the binary tree
+        binaryTree.insert(5);
+        binaryTree.insert(3);
+        binaryTree.insert(7);
+        binaryTree.insert(1);
+        binaryTree.insert(4);
+
+        // Perform in-order traversal of the binary tree
+        System.out.print("In-order traversal: ");
+        binaryTree.inOrderTraversal(binaryTree.root);
     }
 }
