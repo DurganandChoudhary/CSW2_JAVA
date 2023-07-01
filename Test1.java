763181f3b1921ffa8376d726109251d9189f0f25
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-07-01 20:41:45 +0530

    Add files via upload

--- Test1.java
+++ Test1.java
@@ -1,36 +1,25 @@
-// Java implementation of recursive Binary Search
+// Java program to count
+// inversions in an array
+
+import java.io.*;
+
 class Test1 {
+	static int arr[] = new int[] { 1, 20, 6, 4, 5 };
 
-    int binarySearch(int arr[], int l, int r, int x)
-    {
-        if (r >= l) {
-            int mid = l + (r - l) / 2;
+	static int getInvCount(int n)
+	{
+		int inv_count = 0;
+		for (int i = 0; i < n - 1; i++)
+			for (int j = i + 1; j < n; j++)
+				if (arr[i] > arr[j])
+					inv_count++;
 
-            if (arr[mid] == x)
-                return mid;
+		return inv_count;
+	}
 
-
-            if (arr[mid] > x)
-                return binarySearch(arr, l, mid - 1, x);
-
-            return binarySearch(arr, mid + 1, r, x);
-        }
-
-
-        return -1;
-    }
-
-
-    public static void main(String args[])
-    {
-        Test1 ob = new Test1();
-        int arr= new int[]{2, 3, 4, 10, 40};
-        int n = arr.length;
-        int x = 10;
-        int result = ob.binarySearch(arr, 0, n - 1, x);
-        if (result == -1)
-            System.out.println("Element not present");
-        else
-            System.out.println("Element found at index "+ result);
-    }
+	// Driver method to test the above function
+	public static void main(String[] args)
+	{
+		System.out.println("Number of inversions are "+ getInvCount(arr.length));
+	}
 }
