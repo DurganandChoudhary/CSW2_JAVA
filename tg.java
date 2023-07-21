c3864d8a48120562d2c42887bafbaf9f96ce055f
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-07-21 23:17:23 +0530

    Add files via upload

---
+++ tg.java
@@ -0,0 +1,15 @@
+package csw;
+
+import java.time.LocalDate;
+
+class A6qno10 {
+
+    public static void main(String[] args) {
+        LocalDate date=LocalDate.now();
+        System.out.println("Today date "+date);
+        LocalDate tB=date.minusDays(10);
+        System.out.println("Ten day before "+tB);
+        LocalDate tA=date.plusDays(10);
+        System.out.println("Ten day after "+tA);
+    }
+}
