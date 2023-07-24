5d52cac27340ffa8c10673569ceee006e52887f0
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-07-24 23:00:41 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,20 @@
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double nums[]= {10.1,11.2,12.3,13.4,14.5};
+		double result=0;
+		int i;
+		for(int i1=0;i1<5;i1++)
+		{
+			result=result+nums[i1];
+		}
+			//result=result+nums[i1];
+		System.out.println("Average is "+result/5);
+			
+
+	}
+
+}
