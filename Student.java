e037f23ab2530aa94e4b1c8812e3ba57fb9cca30
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-08-25 16:00:48 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,4 +1,4 @@
-
+package April_03_2023;
 import java.util.LinkedList;
 import java.util.ListIterator;
 import java.util.Scanner;
@@ -7,12 +7,12 @@ public class Student {
     private String name;
     private int age;
     private int mark;
-
+   
     public Student(String name, int age, int mark) {
         this.name = name;
         this.age = age;
         this.mark = mark;
-
+      
     }
 
     public String getName() {
@@ -27,7 +27,7 @@ public class Student {
         return mark;
     }
 
-
+  
 
     public static void main(String[] args) {
         LinkedList<Student> students = new LinkedList<>();
@@ -43,27 +43,27 @@ public class Student {
         }
 
         Scanner sc = new Scanner(System.in);
-
-
+        
+        
         System.out.println("Enter a student's name to be dispayed:");
         String name1 = sc.next();
-
+       
         ListIterator<Student> it=students.listIterator();
-        while(it.hasNext()) {
-            Student s = it.next();
-            String name = s.name;
-            System.out.println(s);
-            if(name.equals(name1))
-                System.out.println("true");
-            else
-                System.out.println("false");
-        }
-
-        students.remove(1);
-        for (Student student : students) {
-            System.out.println(student.getName() + " " + student.getAge() + " " + student.getMark()      );
-        }
-        System.out.println(students.size());
+		while(it.hasNext()) {
+			Student s = it.next();
+			String name = s.name;
+			System.out.println(s);
+			if(name.equals(name1))
+				System.out.println("true");
+			else
+				System.out.println("false");
+		}
+	
+        	students.remove(1);
+        	for (Student student : students) {
+                System.out.println(student.getName() + " " + student.getAge() + " " + student.getMark()      );
+            }
+        	 System.out.println(students.size());
     }
 }
 
