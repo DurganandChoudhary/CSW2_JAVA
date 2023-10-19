657e6568fdd12fefe451db98a2021266b95d7139
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-10-20 00:09:33 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,33 +1,37 @@
-abstract class Programming
-{
-	public abstract void Developers();
-}
-class HTML extends Programming
-{
-	//@Override
-	public void Developers()
-	{
-		System.out.println("Tim Berners Lee");
-	}
-}
-class Java extends Programming
-{
-	//@Override
-	public void Developers()
-	{
-		System.out.println("James Gosling");
-	}
-	
-}
-public class Main {
+package Factory;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		HTML h=new HTML();
-		h.Developers();
-		Java j=new Java();
-		j.Developers();
+class Test{
+    int x=10;
+}
+class Demo
+{
+    int y=20;
+    private Demo()
+    {
 
-	}
+    }
+    static Demo getObjectofDemo()
+    {
+        Demo d = new Demo();
+        return d;
+    }
+    Test getObjectofTest()
+    {
+    Test t = new Test();
+    return t;
+    }
+}
 
+class Main {
+    public static void main(String[] args) 
+    {
+        Demo d1 = Demo.getObjectofDemo();
+        System.out.println("The value of y = "+d1.y);
+        System.out.println();
+
+        Test t1 = d1.getObjectofTest();
+        System.out.println("The value of x ="+t1.x);
+        
+    }
+    
 }
