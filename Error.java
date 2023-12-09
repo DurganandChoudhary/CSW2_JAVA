46efc65307af81aed679272c4d8804a542593ed9
DURGANAND CHOUDHARY <102054803+DurganandChoudhary@users.noreply.github.com>
2023-12-10 02:19:18 +0530

    Add files via upload

---
+++ Error.java
@@ -0,0 +1,13 @@
+package Exception;
+
+public class Error 
+{
+    public static void main(String[] args) 
+    {
+        int x = 5;
+        System.out.println(x);
+        
+        System.out.println(5/0);
+    }
+    
+}
